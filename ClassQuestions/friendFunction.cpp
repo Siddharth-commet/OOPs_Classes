78bbd68ffe61df6601d7340338d8097139feda71
Siddharth-commet <sid.siddharth2907@gmail.com>
2025-08-21 01:54:01 +0530

    ref var & friend func

--- ClassQuestions/friendFunction.cpp
+++ ClassQuestions/friendFunction.cpp
@@ -2,3 +2,30 @@
 // cant be defined in a class
 // cant be called with an object
 
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyClass {
+private:
+    int secret;
+
+public:
+    MyClass(int s=10) : secret(s) {}
+
+    // Declaring a friend function
+    friend void revealSecret(MyClass obj);
+};
+
+// Definition of friend function
+void revealSecret(MyClass obj) {
+    // Even though revealSecret is not a member of MyClass,
+    // it can still access private data
+    std::cout << "Secret is: " << obj.secret << std::endl;
+}
+
+int main(){
+    MyClass secretClass(152);
+    revealSecret(secretClass);
+
+    return 0;
+}
--- ClassQuestions/refrenceVariable.cpp
+++ ClassQuestions/refrenceVariable.cpp
@@ -1,6 +1,42 @@
 //it does not have its own identity bu slias to some variable
+// (1) A reference must be initialized when it is declared.
+// (2)Once a reference is bound to a variable, it cannot be changed to refer to another variable:
 
 //&a;
 //*a;
 
-*p = &a;
+
+// 🔹 What is a Reference Variable?
+
+// A reference variable is just another name (alias) for an existing variable.
+// It does not create a new variable; it just gives a new way to refer to the same memory location.
+
+// Declared with the "&" symbol.
+
+// 🔹 Syntax
+// int x = 10;
+// int &ref = x;   // ref is a reference to x
+
+
+// Here, ref is not a new variable.
+
+// Both x and ref refer to the same memory.
+
+// So:
+// ref = 20;   // changes x also
+// cout << x;  // prints 20
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int &b = a; // b is a reference to a
+
+    cout << "a = " << a << ", b = " << b << endl;
+
+    b = 10; // changing b also changes a
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
